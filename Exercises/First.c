8b321f1a133692b93fe2e9f74cdfc1f6a5af9712
SHAK006 <joshshri.k@gmail.com>
2025-01-27 11:26:46 +0530

    Add files via upload

---
+++ Exercises/First.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 20;                
+    float f = 0.2;             
+    char c = 'v';              
+    char arr[15] = "example";  
+
+    printf("Integer value: %d\n", i);          
+    printf("Floating-point value: %.2f\n", f); 
+    printf("Character value: %c\n", c);       
+    printf("String value: %s\n", arr);        
+
+    return 0; 
+}
